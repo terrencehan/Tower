03e6149ec7f0d41aeaa4ab7012d79f7a19d1fc0b
terrencehan <hanliang1990@gmail.com>
2012-09-12 17:22:41 +0800

    add a file containing some constants which will be used in CPU modules

---
+++ lib/Tower/VM/CPU/Constant.pm
@@ -0,0 +1,15 @@
+# lib/Tower/VM/CPU/Constant.pm
+# Copyright (c) 2012 terrencehan
+# hanliang1990@gmail.com
+
+use strict;
+use warnings;
+package Tower::VM::CPU::Constant;
+
+our @ISA    = qw/Exporter/;
+our @EXPORT = qw/ $IHALT $INOP $IRRMOVL $IIRMOVL $IRMMOVL $IMRMOVL $IOPL $IJXX $ICALL $IRET $IPUSHL $IPOPL $FADDL $FSUBL $FANDL $FXORL $FJMP $FJLE $FJL $FJE $FJNE $FJGE $FJG /;
+
+our ($IHALT, $INOP, $IRRMOVL, $IIRMOVL, $IRMMOVL, $IMRMOVL, $IOPL, $IJXX, $ICALL, $IRET, $IPUSHL, $IPOPL) = (0 .. 0xB);
+
+our ($FADDL, $FSUBL, $FANDL, $FXORL) = (0 .. 3); 
+our ($FJMP, $FJLE, $FJL, $FJE, $FJNE, $FJGE, $FJG) = (0 .. 6);
