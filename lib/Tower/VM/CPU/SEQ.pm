c8df0d8878808db393d052b5d13954dad7d91593
terrencehan <hanliang1990@gmail.com>
2012-09-15 09:00:25 +0800

    finish sequence cpu

--- lib/Tower/VM/CPU/SEQ.pm
+++ lib/Tower/VM/CPU/SEQ.pm
@@ -1,4 +1,255 @@
+# lib/Tower/VM/CPU/SEQ.pm
+# Copyright (c) 2012 terrencehan
+# hanliang1990@gmail.com
+
+package Tower::VM::CPU::SEQ;
+
 use strict;
 use warnings;
-package Tower::VM::CPU::SEQ;
+use 5.010;
+
+
+use Tower::VM::CPU;
+use Tower::VM::CPU::Constant;
+use Tower::Tool::Complement qw/to_decimal/;
+use Tower::VM::Memory::Helper qw/
+                        put_val_in_4bytes
+                        get_val
+                        /;
+
+our @ISA = qw/Tower::VM::CPU/;
+
+sub new{
+    my $class = shift;
+    my $self = Tower::VM::CPU->new;
+    my %hash = (
+        valP        => 0x0,
+        valA        => 0x0,
+        valB        => 0x0,
+        valC        => 0x0,
+        valE        => 0x0,	
+        valM        => 0x0,
+        rA 	        => 0x0,
+        rB 	        => 0x0,
+        icode       => 0x0,
+        ifun        => 0x0,
+        %{$self}
+    );
+    $self = \%hash;
+    return bless $self,$class; 
+}
+
+sub run_next_instruction{
+    my ($self, $mem) = @_;
+
+    my @six_bytes						= $mem->read($self->{pc}, 6);
+    for (@six_bytes) {$_ = 0 unless defined $_;}
+    my ($icode, $ifun, $rA, $rB, @valC)	= @six_bytes;
+
+
+    $self->{valC}	= to_decimal (join "", @valC);
+    $self->{icode}	= hex $icode;
+    $self->{ifun}	= hex $ifun;
+    $self->{rA}		= hex $rA;
+    $self->{rB}		= hex $rB;
+    $self->{valA} 	= $self->get_r_with_num($rA);
+    $self->{valB} 	= $self->get_r_with_num($rB);
+
+    given($self->{icode}){
+        when($INOP)		{$self->handle_nop}
+        when($IHALT)	{$self->handle_halt}
+        when($IRRMOVL)	{$self->handle_rrmovl}
+        when($IIRMOVL)	{$self->handle_irmovl}
+        when($IRMMOVL)	{$self->handle_rmmovl($mem)}
+        when($IMRMOVL)	{$self->handle_mrmovl($mem)}
+        when($IOPL)		{$self->handle_opl}
+        when($IJXX)		{$self->handle_jXX($mem)}
+        when($ICALL)	{$self->handle_call($mem)}
+        when($IRET)		{$self->handle_ret($mem)}
+        when($IPUSHL)	{$self->handle_pushl($mem)}
+        when($IPOPL)	{$self->handle_popl($mem)}
+    }
+
+}
+
+
+
+sub handle_halt{
+    my $self = shift;
+    $self->{stat} = 123;
+}
+
+#sub handle_nop{
+	#say "handle_nop{";
+
+#}
+
+sub handle_rrmovl{
+    my $self = shift;
+    $self->{valP} = $self->{pc} + 2;
+    $self->{valE} = 0 + $self->{valA};
+    $self->put_r_with_num($self->{rB}, $self->{valE});
+    $self->{pc} = $self->{valP};
+}
+
+sub handle_irmovl{
+    my $self = shift;
+    $self->{valP} = $self->{pc} + 6;
+    $self->{valE} = 0 + $self->{valC};
+    $self->put_r_with_num($self->{rB}, $self->{valE});
+    $self->{pc} = $self->{valP};
+}
+
+sub handle_rmmovl{
+    my ($self, $mem) = @_;
+    $self->{valP} = $self->{pc} + 6;
+    $self->{valE} = $self->{valB} + $self->{valC};
+
+    put_val_in_4bytes($mem, $self->{valE}, $self->{valA});
+    $self->{pc} = $self->{valP};
+}
+
+sub handle_mrmovl{
+    my ($self, $mem) = @_;
+    $self->{valP} = $self->{pc} + 6;
+    $self->{valE} = $self->{valB} + $self->{valC};
+    $self->{valM} = get_val($mem, $self->{valE}, 4);
+    $self->put_r_with_num($self->{rA}, $self->{valM});
+    $self->{pc} = $self->{valP};
+}
+
+#sub set_cc{
+#}
+
+sub handle_opl{
+    my $self = shift;
+    $self->{valP} = $self->{pc} + 2;
+    
+    given ( $self->{ifun} ) {
+        when($FADDL){ $self->{valE} = $self->{valB} + $self->{valA}; }
+        when($FSUBL){ $self->{valE} = $self->{valB} - $self->{valA}; }
+        when($FANDL){ $self->{valE} = $self->{valB} & $self->{valA}; }
+        when($FXORL){ $self->{valE} = $self->{valB} ^ $self->{valA}; }
+    }
+
+    $self->{cc}->{"SF"} = 0;
+    $self->{cc}->{"ZF"} = 0;
+    $self->{cc}->{"OF"} = 0;
+    $self->{cc}->{"OF"} = 1 
+            if($self->{valE} > to_decimal("7fffffff") 
+            or $self->{valE} < to_decimal("80000000"));
+    $self->{cc}->{"ZF"} = 1 if($self->{valE} == 0x0);
+    $self->{cc}->{"SF"} = 1 if($self->{valE} < 0);
+
+    $self->put_r_with_num($self->{rB}, $self->{valE});
+    $self->{pc} = $self->{valP};
+}
+
+sub handle_jXX{
+    my ($self, $mem) = @_;
+    $self->{valC} = get_val $mem, $self->{pc} + 1, 4; 
+    $self->{valP} = $self->{pc} + 5;
+
+    $self->{cnd} = 0;
+    given($self->{ifun}){
+        when($FJMP){$self->{cnd}    = 1}
+        when($FJLE){$self->{cnd}    = ($self->{cc}->{"ZF"} == 1 or $self->{cc}->{"SF"} == 1)}
+        when($FJL){$self->{cnd}     = ($self->{cc}->{"ZF"} == 0 and $self->{cc}->{"SF"} == 1)}
+        when($FJE){$self->{cnd}     = ($self->{cc}->{"ZF"} == 1)}
+        when($FJNE){$self->{cnd}    = ($self->{cc}->{"ZF"} == 0)}
+        when($FJGE){$self->{cnd}    = ($self->{cc}->{"ZF"} == 1 or $self->{cc}->{"SF"} == 0)}
+        when($FJG){$self->{cnd}     = ($self->{cc}->{"ZF"} == 0 and $self->{cc}->{"SF"} == 0)}
+    }
+
+    $self->{pc} = $self->{cnd} ? $self->{valC} : $self->{valP};
+}
+
+sub handle_call{
+    my ($self, $mem) = @_;
+    $self->{valC} = get_val $mem, $self->{pc} + 1, 4;
+    $self->{valP} = $self->{pc} + 5;
+    $self->{valB} = $self->{res}->{"%esp"};
+    $self->{valE} = $self->{valB} - 4;
+
+    put_val_in_4bytes $mem, $self->{valE}, $self->{valP};
+    $self->{res}->{"%esp"} = $self->{valE};
+
+    $self->{pc} = $self->{valC};
+}
+
+sub handle_ret{
+    my ($self, $mem) = @_;
+    $self->{valP} = $self->{pc} + 1;
+    $self->{valA} = $self->{res}->{"%esp"};
+    $self->{valB} = $self->{res}->{"%esp"};
+    $self->{valE} = $self->{valB} + 4;
+
+    $self->{valM} = get_val $mem, $self->{valA}, 4;
+
+    $self->{res}->{"%esp"} = $self->{valE};
+
+    $self->{pc} = $self->{valM};
+}
+
+sub handle_pushl{
+    my ($self, $mem) = @_;
+    $self->{valP} = $self->{pc} + 2;
+    $self->{valB} = $self->{res}->{"%esp"};
+    $self->{valE} = $self->{valB} - 4;
+    put_val_in_4bytes $mem, $self->{valE}, $self->{valA};
+    $self->{res}->{"%esp"} = $self->{valE};
+    $self->{pc} = $self->{valP};
+}
+
+sub handle_popl{
+    my ($self, $mem) = @_;
+    $self->{valP} = $self->{pc} + 2;
+    $self->{valA} = $self->{res}->{"%esp"};
+    $self->{valB} = $self->{res}->{"%esp"};
+    $self->{valE} = $self->{valB} + 4;
+    $self->{valM} = get_val $mem, $self->{valA}, 4;
+    $self->{res}->{"%esp"} = $self->{valE};
+    $self->put_r_with_num($self->{rA}, $self->{valM});
+    $self->{pc} = $self->{valP};
+}
+
+sub start{
+    my ($self, $mem, $pc_start_position, $show_cpu_info) = @_;
+    my $count = 1;
+    $pc_start_position = 0x00 unless (defined $pc_start_position);
+    $self->{pc} = $pc_start_position;
+    while($self->{stat} != 123){
+        $self->run_next_instruction($mem);
+        if($show_cpu_info){
+            $self->print_cpu_info;
+            say "----------------------instruction ".$count++." done";
+        }
+    }
+}
+
+sub _get_hex_form{
+	my $val = shift;
+	my  $str = sprintf "%.8x", $val;
+	$str = substr($str, 8) if length($str) > 8;
+	return $str; #shortroute
+	return $val;
+}
+
+sub print_cpu_info{
+    my $self = shift;
+    say "icode:	". _get_hex_form($self->{icode});
+    say "ifun: 	". _get_hex_form($self->{ifun});
+    say "rA:	". _get_hex_form($self->{rA});
+    say "rB:	". _get_hex_form($self->{rB});
+    say "valA:	". _get_hex_form($self->{valA});
+    say "valB:	". _get_hex_form($self->{valB});
+    say "valC:	". _get_hex_form($self->{valC});
+    say "valE:	". _get_hex_form($self->{valE});
+    say "valM:	". _get_hex_form($self->{valM});
+    say "valP:	". _get_hex_form($self->{valP});
+    say "PC:	". _get_hex_form($self->{pc});
+    $self->print_cc;
+    $self->print_res;
+}
+
 1;
--- t/04-vm-cpu-seq-dev.t
+++ t/04-vm-cpu-seq-dev.t
@@ -1,6 +1,189 @@
-#use strict;
-#use warnings;
+# 04-vm-cpu-seq-dev.t
+# Copyright (c) 2012 terrencehan
+# hanliang1990@gmail.com
+
+use strict;
+use warnings;
 
 use lib '../lib';
 use Test::More 'no_plan';
-BEGIN {use_ok 'Tower::VM::CPU::Pipe'};
+
+use Tower::VM::Memory;
+use Tower::VM::CPU::Constant;
+use Tower::VM::Memory::Helper qw/ get_val put_val_in_4bytes /;
+
+BEGIN {use_ok 'Tower::VM::CPU::SEQ'};
+
+my $seq = Tower::VM::CPU::SEQ->new;
+isa_ok $seq, 'Tower::VM::CPU::SEQ';
+isa_ok $seq, 'Tower::VM::CPU';
+
+#for (keys %{$seq}){
+    #print $_."\n";
+#}
+
+$seq->handle_halt;
+is $seq->{stat}, 123, "handle_halt";
+
+for (0 .. 7){
+    $seq->{pc}     = $_;
+    $seq->{valC}   = $_;
+    $seq->{rB}     = $_;
+    $seq->handle_irmovl;
+    no strict;
+    is $seq->{res}->{ $seq->{res_num_to_name}->{$_} }, $_, "handle_irmovl";
+    is $seq->{pc}, $_ + 6,"handle_irmovl";
+}
+
+for (0 .. 3){
+    ($seq->{rA}, $seq->{rB}) = ($_, 7 - $_); 
+    $seq->{valA} = $seq->get_r_with_num($seq->{rA});
+    $seq->handle_rrmovl;
+}
+for (0 .. 3){
+    is $seq->{res}->{ $seq->{res_num_to_name}->{$_} }, $_, "handle_rrmovl";
+}
+for (4 .. 7){
+    is $seq->{res}->{ $seq->{res_num_to_name}->{$_} }, 7 - $_, "handle_rrmovl";
+}
+
+
+my $mem = Tower::VM::Memory->new;
+
+#set the value of register as 0 to 7 in order of the SN of Res. Then call handle_rmmovl, write all this number into memory. Check it with Tower::VM::Memory::Helper::get_val
+for (0 .. 7){
+    $seq->{pc}     = $_;
+    $seq->{valC}   = $_;
+    $seq->{rB}     = $_;
+    $seq->handle_irmovl;
+
+    $seq->{valB}   = 0;
+    $seq->{valC}   = 4 * $_;
+    $seq->{valA}   = $_;
+    $seq->handle_rmmovl($mem);
+    is get_val($mem, 4 * $_, 4), $_, "handle_rmmovl";
+}
+
+#set all the value of regitster as 0;
+for (0 .. 7){
+    no strict;
+    $seq->{valC}   = 0;
+    $seq->{rB}     = $_;
+    $seq->handle_irmovl;
+}
+
+for (0 .. 7){
+    $seq->{valB}    = 0;
+    $seq->{valC}    = 4 * $_;
+    $seq->{rA}      = $_;
+    $seq->handle_mrmovl($mem);
+    is $seq->get_r_with_num($_), $_, "handle_mrmovel";
+}
+
+#now the value of registers is 0 .. 7 in the order or sequence of number
+
+$seq->{ifun} = $FSUBL;
+$seq->{valA} = 8;
+$seq->{valB} = 1;
+$seq->{rB} = 0;
+$seq->handle_opl;
+is $seq->get_r_with_num(0), -7, "handle_opl";
+is $seq->{cc}->{"ZF"}, 0, "handle_opl";
+is $seq->{cc}->{"SF"}, 1, "handle_opl";
+
+
+put_val_in_4bytes $mem, 1, 0x100;
+$seq->{pc}   = 0;
+$seq->{ifun} = $FJMP;
+$seq->handle_jXX($mem);
+is $seq->{pc}, 0x100, "handle_jXX";
+
+$seq->{pc}   = 0;
+$seq->{ifun} = $FJGE;
+$seq->{cc}->{'ZF'} = 1;
+$seq->{cc}->{'SF'} = 0;
+$seq->handle_jXX($mem);
+is $seq->{pc}, 0x100, "handle_jXX";
+
+put_val_in_4bytes $mem, 1, 0x55;
+$seq->{res}->{"%esp"} = 0x100;
+$seq->{pc}   = 0x00;
+$seq->handle_call($mem);
+is $seq->{pc}, 0x55, "handle_call";
+is $seq->{res}->{"%esp"}, 0xfc, "handle_call";
+is get_val($mem, 0xfc, 4), 0x05, "handle_call";
+
+#handle_ret test is following the handle_call above.
+$seq->handle_ret($mem);
+is $seq->{pc}, 0x05, "handle_ret";
+
+$seq->{pc} = 0;
+$seq->{res}->{"%esp"} = 0x100;
+$seq->{valA} = 0xabc;
+$seq->handle_pushl($mem);
+is $seq->{pc}, 2, "handle_pushl";
+is get_val($mem, $seq->{res}->{"%esp"}, 4), 0xabc, "handle_pushl";
+
+#handle_pop test is following the handle_pushl above.
+$seq->{pc} = 0;
+$seq->{rA} = 0;
+$seq->handle_popl($mem);
+is $seq->{pc}, 2, "handle_popl";
+is $seq->get_r_with_num(0), 0xabc, "handle_popl";
+
+
+$seq = Tower::VM::CPU::SEQ->new;
+#$mem->write_str(0, 6, "30f400000110");
+$mem->write_str(0, 7, "30f40000011000");
+$seq->run_next_instruction($mem);
+is $seq->{res}->{"%esp"}, 0x110, "run_next_instruction";
+$seq->run_next_instruction($mem);
+is $seq->{stat}, 123, "run_next_instruction";
+$seq->{pc} = 0x00;
+$seq->run_next_instruction($mem);
+is $seq->{res}->{"%esp"}, 0x110, "run_next_instruction";
+
+$seq = Tower::VM::CPU::SEQ->new;
+my $byte_code = join "", split "\n", do{local $/; <DATA>};
+$mem->write_str(0, (length $byte_code)/2, $byte_code);
+$seq->start($mem, 0x00);
+is $seq->{stat}, 123, "start";
+is $seq->{res}->{"%eax"}, 0xabcd, "start";
+
+__DATA__
+30f400000100
+30f500000100
+8000000024
+00
+0000
+0000000d
+000000c0
+00000b00
+0000a000
+a05f
+2045
+30f000000004
+a00f
+30f200000014
+a02f
+8000000042
+2054
+b05f
+90
+a05f
+2045
+501500000008
+50250000000c
+6300
+6222
+7300000078
+506100000000
+6060
+30f300000004
+6031
+30f3ffffffff
+6032
+740000005b
+2054
+b05f
+90
