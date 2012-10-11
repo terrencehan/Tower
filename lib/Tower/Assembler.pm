91e8c21a4ab903acab46b6320edfa8098993367f
terrencehan <hanliang1990@gmail.com>
2012-10-11 11:39:52 +0800

    none

--- lib/Tower/Assembler.pm
+++ lib/Tower/Assembler.pm
@@ -7,12 +7,37 @@ use warnings;
 
 package Tower::Assembler;
 
-sub new {
-    my $class = shift;
-    my $self  = {
+our @ISA = qw/Exporter/;
 
-    };
-    return bless $self, $class;
+our @EXPORT_OK = qw/get_file/;
+our @EXPORT    = qw/rough_handle_src/;
+
+my %res = (
+    "%eax" => 0x0,
+    "%ecx" => 0x1,
+    "%edx" => 0x2,
+    "%ebx" => 0x3,
+    "%esp" => 0x4,
+    "%ebp" => 0x5,
+    "%esi" => 0x6,
+    "%edi" => 0x7,
+);
+
+my @instr =
+  qw( rrmovl cmovle cmovl cmove cmovne cmovge cmovg rmmovl mrmovl irmovl addl subl andl xorl jmp jle jl je jne jge jg call ret pushl popl .byte .word .long .pos .align halt nop iaddl leave);
+
+sub get_file {
+    my $file = shift;
+    open my $in, "<", $file or die "can not open this file: $file";
+    my $src = do { local $/; <$in> };
+}
+
+sub rough_handle_src {
+    my $src = shift;
+    $src =~ s/#.*$//gm;
+    $src =~ s/^\s*\n//gm;
+    print $src;
+    print split /\n/, $src;
 }
 
 1;
--- t/08-assembler.t
+++ t/08-assembler.t
@@ -1,4 +1,4 @@
-# t/07-assembler.t
+#line t/07-assembler.t
 # Copyright (c) 2012 terrencehan
 # hanliang1990@gmail.com
 
@@ -9,7 +9,8 @@ use lib '../lib';
 use Test::Base;
 use Test::More 'no_plan';
 use Test::Deep;
-use Tower::Assembler;
+use Tower::Assembler qw/get_file/;
 
 BEGIN { use_ok('Tower::Assembler'); }
 
+rough_handle_src (get_file "./asumr.ys");
